635cdd657906b5527fc0813918c60b83169a9fd3
niduniDK <nidunikasige@gmail.com>
2025-04-26 17:21:11 +0530

    Add build ast function

--- AST.py
+++ AST.py
@@ -18,3 +18,31 @@ class AST:
     def build_AST(self):
         if not self.node_values:
             return []
+        self.setRoot()
+        current_node = self.root
+        i = 0
+        while i<len(self.node_values)-2:
+            current_node.value = self.node_values[i]
+            if len(self.node_values)-1 >= 2*i+1 :
+                left = Node(self.node_values[2*i + 1])
+            else:
+                left = None
+            if len(self.node_values)-1 >= 2*i+2:
+                right = Node(self.node_values[2*i + 2])
+            else:
+                right = None
+            current_node.left = left
+            current_node.right = right
+            current_node = current_node.left
+            if i%2 != 0:
+                i = 2*i + 1
+            else:
+                i += 1
+
+    
+    def pre_order_traverse(self, node):
+        current_node = node
+        if current_node is not None:
+            print(current_node.value, end=' ')
+            self.pre_order_traverse(current_node.left)
+            self.pre_order_traverse(current_node.right)
