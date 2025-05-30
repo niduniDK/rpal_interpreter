f3cb5ca4644a0a21a159247c36b778634b80cc78
Raleesa Perera <raleesasvperera@gmail.com>
2025-05-31 00:28:28 +0530

    add rule 8 to CSE_machine

--- CSE_machine.py
+++ CSE_machine.py
@@ -23,15 +23,23 @@ class CSEMachine:
                 if instr == 'gamma':  # CSE Rule 3 or Rule 4
                     self._apply_gamma()
 
-                elif instr in ['+', '-', '*', '/', 'eq', 'lt', 'not']:
+                elif instr == "β":
+                    self._apply_beta()
+
+                elif instr in ['+', '-', '*', '/', 'eq', 'le','not','ls','neg','gr','ge','ne']:
                     if instr == 'not' or instr == 'neg':
-                        operand = self.stack.pop()
-                        result = apply_operator(instr, None, operand)
+                        #operand = self.stack.pop()
+                        result = apply_operator(instr, None, None)
+
                     else:
                         right = self.stack.pop()
                         left = self.stack.pop()
-                        self.control.pop()
-                        self.control.pop()
+                        #self.control.pop()
+                        #self.control.pop()
+                        if self.control and self.control[-1] == 'gamma':
+                            self.control.pop()  
+                            if self.control:
+                                self.control.pop() 
                         result = apply_operator(instr, left, right)
                     self.stack.append(result)
 
@@ -55,16 +63,19 @@ class CSEMachine:
                             elif tag_type == 'STR':
                                 val = eval(raw_val)  # handles quotes, e.g. "'hello'"
                             elif tag_type == 'ID':
-                                # Treat as variable identifier – look it up
-                                val = lookup(raw_val, self._current_env())
-                                self.stack.append(val)
+                                new_val ="<ID:"+raw_val+">"
+                                if new_val == "<ID:print>" :
+                                    self._apply_print()
+                                else:
+                                    val = lookup(new_val, self._current_env())
+                                #self.stack.append(val)
                             else:
                                 raise Exception(f"Unsupported literal type: {tag_type}")
                             self.stack.append(val)
                         except Exception as e:
                             raise Exception(f"Failed to parse tagged literal {instr}: {e}")
                     else:
-                        # CSE Rule 1: Variable name
+                        
                         val = lookup(instr, self._current_env())
                         self.stack.append(val)
 
@@ -78,7 +89,7 @@ class CSEMachine:
                 tuple_values = [self.stack.pop() for _ in range(n)]
                 self.stack.append(tuple(reversed(tuple_values)))
 
-            elif isinstance(instr, str) and instr in ['+', '-', '*', '/', 'eq', 'lt', 'not']:  # Operators
+            elif isinstance(instr, str) and instr in ['+', '-', '*', '/', 'eq', 'le', 'not','ls','neg','gr','ge','ne']:  # Operators
                 right = self.stack.pop()
                 left = self.stack.pop() if instr != 'not' else None
                 result = apply_operator(instr, left, right)
@@ -89,6 +100,7 @@ class CSEMachine:
                 self.stack.append(instr)
         print(self.stack)
         return self.stack[-1]
+    
 
     def _apply_gamma(self):
         if len(self.stack) < 2:
@@ -121,3 +133,28 @@ class CSEMachine:
             # Rule 3 (apply operator or built-in function)
             result = apply_operator("gamma", rator, rand)
             self.stack.append(result)
+
+    def _apply_beta(self):
+        condition = self.stack.pop()
+
+        if len(self.control) < 2:
+            raise Exception("Control underflow: Not enough elements to apply β")
+
+        false_branch = self.control.pop()
+        true_branch = self.control.pop()
+
+        if condition == 'true':
+            chosen_branch = true_branch
+        else:
+            chosen_branch = false_branch
+
+        if chosen_branch not in self.control_structures:
+            raise Exception(f"Missing control structure: {chosen_branch}")
+
+        new_control = list(self.control_structures[chosen_branch])
+        self.control.extend(new_control)
+
+    def _apply_print(self):
+        self.control.pop()
+        print("This is what I got now :\ncontrol",self.control,"and stack",self.stack)
+        self.stack.pop()
