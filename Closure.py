4a9b82dd09d56ef44dcb9078a2793030e3fb9d31
niduniDK <nidunikasige@gmail.com>
2025-06-02 10:14:50 +0530

    Add new closure for eta

--- Closure.py
+++ Closure.py
@@ -6,3 +6,13 @@ class Closure:
 
     def __repr__(self):
         return f"<Closure δ{self.delta_index} λ {self.var} env={self.env}>"
+    
+
+class ClosuerEta:
+    def __init__(self, delta_index, var, env):
+        self.delta_index = delta_index
+        self.var = var
+        self.env = env
+
+    def __repr__(self):
+        return f"<Closure δ{self.delta_index} η {self.var} env={self.env}>"
